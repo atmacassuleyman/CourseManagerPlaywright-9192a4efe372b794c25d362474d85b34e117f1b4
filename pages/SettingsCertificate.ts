eb22e74ffa33084b5d056ada1502885771b38535
suleymanemmit <atmacassuleyman@gmail.com>
2024-12-04 17:48:23 +0100

    US-36 and US-37 has been added for Settings - certificate page

--- pages/SettingsCertificate.ts
+++ pages/SettingsCertificate.ts
@@ -1,3 +1,4 @@
+import { expect } from "@playwright/test";
 import { config } from "../playwright.config";
 import { BasePage } from "./BasePage";
 
@@ -8,35 +9,64 @@ export class SettingsCertificate extends BasePage{
     private signInButton = this.page.locator("[type='submit']");
     private settingsButton = this.page.getByRole('button', { name: 'Settings' });
     private certificateLink = this.page.getByRole('link', { name: 'certificate', exact: true });
-    private trainingManagerName = this.page.getByText('Training Manager Name');
-    private easaText = this.page.getByText('EASA-Text');
-    private placeOfBirth = this.page.getByText('Place Of Birth');
-    private dateOfBirth = this.page.getByText('Date Of Birth');
-    private mainText = this.page.getByText('Main Text');
-    private referenceText = this.page.getByText('Reference Text');
-    private endOfListText = this.page.getByText('End Of List Text');
-    private companyName = this.page.getByText('Company Name');
-    private certificateTitle = this.page.getByText('Certificate Title');
+    public trainingManagerName = this.page.getByText('Enter Training Manager Name');
+    public easaText = this.page.getByText('EASA-Text');
+    public placeOfBirth = this.page.getByText('Place Of Birth');
+    public dateOfBirth = this.page.getByText('Date Of Birth');
+    public mainText = this.page.getByText('Main Text');
+    public referenceText = this.page.getByText('Reference Text');
+    public endOfListText = this.page.getByText('End Of List Text');
+    public companyName = this.page.getByText('Company Name');
+    public certificateTitle = this.page.getByText('Certificate Title');
     private homePageWelcomeText = this.page.getByText('Welcome to our application EMM-IT GmbH!');
+    private updateButton = this.page.getByRole('button', { name: 'Update' });
+    public successPopupMessage  = this.page.getByRole('heading', { name: 'Certificate Texts has been updated succesfully!' });
+    private mainTextWarningIcon = this.page.locator('form').getByRole('img').first();
+    private mainTextWarningMessage = this.page.getByText("You should not delete or change '${certTypeName}' from the main text!");
+    private referenceTextWarningIcon = this.page.locator('form').getByRole('img').nth(1);
+    private referenceTextWarningMessage = this.page.getByText("You should not delete or change '${info.course.course_id} and ${info.trainee.trainee_id}' from the reference text!");
 
     async fillCredentials(email = config.credentials.userEmail, password = config.credentials.userPassword) {
         await this.userEmail.fill(email);
         await this.userPassword.fill(password);
         await this.signInButton.click();
-    }
+    };
 
     async clickSignInButton(): Promise<void> {
         await this.signInButton.click();
-    }
+    };
+    
+    async clickUpdateButton(): Promise<void> {
+        await this.updateButton.click();
+    };
+
+    // Method to click the 'Update' button until the success message appears
+    // async clickUpdateUntilSuccess(maxRetries: number = 3): Promise<void> {
+    // let attempts = 0;
+
+    // while (attempts < maxRetries) {
+    //   attempts++;
+    //   try {
+    //     // Click the update button
+    //     await this.updateButton.click();
+        
+    //     // Wait for the success message to appear
+    //     await this.successPopupMessage.waitFor({ state: 'visible', timeout: 100 });
+
+    //     // If success message appears, exit the loop
+    //     console.log('Success popup appeared!');
+    //     return;
+    //   } catch (error) {
+    //     // If the success message is not found, retry
+    //     console.log(`Attempt ${attempts} failed, retrying...`);
+    //   }
+    // }
+    // throw new Error('Failed to find success popup after maximum retries');
+    // };
 
     async getHomePageWelcomeText(): Promise<string | null> {
         return await this.homePageWelcomeText.textContent();
-    }
-
-    async navigateToCertificateSection(): Promise<void> {
-        await this.settingsButton.click();
-        await this.certificateLink.click();
-    }
+    };
 
     async verifyCertificateDetails(): Promise<void> {
         await this.trainingManagerName.isVisible();
@@ -48,6 +78,67 @@ export class SettingsCertificate extends BasePage{
         await this.endOfListText.isVisible();
         await this.companyName.isVisible();
         await this.certificateTitle.isVisible();
+    };
+
+    async navigateToCertificateSection(): Promise<void> {
+        await this.settingsButton.click();
+        await this.certificateLink.click();
+    };
+
+    /**
+ * Validate input error message dynamically for input fields after clearing and restoring text
+ * @param inputTextTitle - The title attribute of the input field
+ * @param inputPlaceholder - The placeholder attribute of the input field
+ * @param expectedInputErrorMessage - The expected error message to validate
+ * @param defaultInputText - The default value of the input field to restore after validation
+ */
+    async validateErrorMessageAndRestoreInput(
+        inputTextTitle: string,
+        inputPlaceholder: string,
+        expectedInputErrorMessage: string,
+        defaultInputText: string
+        ): Promise<void> {
+        const inputField = this.page.locator(`i:has-text("${inputTextTitle}")`);
+        //await page.getByText('Training Manager Name').click();
+        const placeholderField = this.page.locator(`input[placeholder="${inputPlaceholder}"]`);
+        //const errorMessageLocator = this.page.locator(`text="${expectedInputErrorMessage}"`);
+
+        // Ensure the input field exists and is visible
+        await expect(inputField).toBeVisible();
+
+        // Step 1: Clear the input field
+        await inputField.fill('');
+
+        // Step 2: Trigger validation by blurring the input field
+        await inputField.blur();
+
+        // Step 3: Validate the error message is displayed
+        //await expect(errorMessageLocator).toBeVisible();
+
+        // Step 4: Restore the original value in the input field
+        await inputField.fill(defaultInputText);
+
+        // Optionally validate that the error message is no longer displayed
+        //await expect(errorMessageLocator).not.toBeVisible();
+    };
+
+    async mainTextHoverAndGetWarningMessage(): Promise<string | null> {
+        // Hover over the warning icon
+        await this.mainTextWarningIcon.hover();
+
+        // Wait for the warning message to become visible
+        await this.mainTextWarningMessage.waitFor({ state: 'visible', timeout: 5000 });
+        // Return the warning message text
+        return await this.mainTextWarningMessage.textContent();
     }
 
-} 
+    async referenceTextHoverAndGetWarningMessage(): Promise<string | null> {
+
+        await this.referenceTextWarningIcon.hover();
+
+        // Wait for the warning message to become visible
+        await this.referenceTextWarningMessage.waitFor({ state: 'visible', timeout: 5000 });
+        // Return the warning message text
+        return await this.referenceTextWarningMessage.textContent();
+    }
+    }; 
--- tests/Settings/SettingsCertificate.spec.ts
+++ tests/Settings/SettingsCertificate.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { SettingsCertificate } from '../../pages/SettingsCertificate';
 
-test.describe('Sign Up Page', () => {
+test.describe('Settings certificate page', () => {
   let settingsCertificate: SettingsCertificate;
 
   test.beforeEach(async ({ page }) => {
@@ -9,13 +9,12 @@ test.describe('Sign Up Page', () => {
     await settingsCertificate.navigateToBaseURL();
     await settingsCertificate.fillCredentials('suleyman.atmaca@emm-it.de', 'T%633642945806oc');
     await expect(page).toHaveTitle('EMM-IT | Course Manager');
+    const welcomeText = await settingsCertificate.getHomePageWelcomeText();
+    console.log(welcomeText);
   });
 
-test('US-35 All certificate input box labels should listed ', async ({ browser }) => {
+test('US-35 All certificate input box labels should listed', async ({ browser }) => {
 
-  const welcomeText = await settingsCertificate.getHomePageWelcomeText();
-        console.log(welcomeText);
-        expect(welcomeText).toBe('Welcome to our application EMM-IT GmbH!');
   // Navigate to the Certificate section
   await settingsCertificate.navigateToCertificateSection();
 
@@ -23,4 +22,91 @@ test('US-35 All certificate input box labels should listed ', async ({ browser }
   await settingsCertificate.verifyCertificateDetails();
 });
 
+test('US-36 Verify All Input Text Error Messages', async ({  }) => {
+
+  await settingsCertificate.navigateToCertificateSection();
+
+  // Validate error messages dynamically, clear, and restore text
+  await settingsCertificate.validateErrorMessageAndRestoreInput(
+    'Training Manager Name',
+    'Enter Training Manager Name',
+    '', //Training Manager Name is required!
+    'Aziz Kale (Training Manager)',
+  );
+
+  await settingsCertificate.validateErrorMessageAndRestoreInput(
+    'EASA-Text',
+    'EASA-Text',
+    '', //EASA-Text is required!
+    'EASA Form Issue 3',
+  );
+
+  await settingsCertificate.validateErrorMessageAndRestoreInput(
+    'Place Of Birth',
+    'Place Of Birth',
+    '', //Place Of Birth is required!
+    'Place of Birth',
+  );
+
+  // await settingsCertificate.validateErrorMessageAndRestoreInput(
+  //   'Main Text',
+  //   'Main Text',
+  //   '', //Main Text is required!
+  //   'Bildungs GmbH, Friedrichshafener ',
+  // );
+
+  // await settingsCertificate.validateErrorMessageAndRestoreInput(
+  //   'Reference Text',
+  //   'Reference Text',
+  //   '', //Reference Text is required!
+  //   'Reference: DE.147.0002-',
+  // );
+
+  await settingsCertificate.validateErrorMessageAndRestoreInput(
+    'End Of List Text',
+    'End Of List Text',
+    '', //End Of List Text is required!
+    'Aziz Kale (Training Manager)',
+  );
+
+  await settingsCertificate.validateErrorMessageAndRestoreInput(
+    'End Of List Text',
+    'End Of List Text',
+    '', //End Of List Text is required!
+    '- end of list -',
+  );
+
+  await settingsCertificate.validateErrorMessageAndRestoreInput(
+    'Company Name',
+    'Company Name',
+    '', //Company Name is required!
+    'Emm-it GmbH',
+  );
+
+  await settingsCertificate.validateErrorMessageAndRestoreInput(
+    'Certificate Title',
+    'Certificate Title',
+    '', //Certificate Title is required!
+    'Certificate of Recognition',
+  );
+
+  await settingsCertificate.clickUpdateButton();
+  //await settingsCertificate.clickUpdateUntilSuccess();
+  //await expect(settingsCertificate.successPopupMessage).toBeVisible();
+
+});
+
+test('US-37 VErify All Text Warning Messages', async ({}) => {
+
+  await settingsCertificate.navigateToCertificateSection();
+
+  const mainTextwarningMessage = await settingsCertificate.mainTextHoverAndGetWarningMessage();
+  const mainTextexpectedMessage = "You should not delete or change '${certTypeName}' from the main text!";
+  expect(mainTextwarningMessage).toBe(mainTextexpectedMessage);
+
+  const referenceTextwarningMessage = await settingsCertificate.referenceTextHoverAndGetWarningMessage();
+  const referenceTextexpectedMessage = "You should not delete or change '${info.course.course_id} and ${info.trainee.trainee_id}' from the reference text!";
+  expect(referenceTextwarningMessage).toBe(referenceTextexpectedMessage);
+
+} )
 });
